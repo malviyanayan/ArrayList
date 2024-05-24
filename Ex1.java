c1ee5def0b54b789134dd9baedfc6ded8262e89f
nayan malviya <naymalviya@gmail.com>
2024-05-24 21:11:17 +0530

    This is my Array list reposetories

---
+++ Ex1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/* This is my First ArrayList program */ 
+
+class Ex1{
+    public static void main(String[] args){
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(2);
+        a.add(4);
+
+        System.out.println("My array list is : "+a);
+
+    }
+}
---
+++ Ex3.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+
+/* implementing all methods of ArrayList */
+
+
+
+/*
+   All implementing methods from Collection interface
+   * add()  -  Appends the specified element to the end of this list.
+   * remove()  - Removes the first occurrence of the specified element from this list, if it is present.
+   * contains()
+   * size()
+   * isEmpty()
+
+   * iterator() -> Ex3.java
+*/
+
+
+class Ex2{
+    public static void main(String[] args){
+        ArrayList<Character> list = new ArrayList<Character>();
+        
+        list.add('a');
+        list.add('b');
+        list.add('c');
+        list.add('d');
+        list.add('e');
+        list.add('f');
+        list.add('a');
+
+        System.out.println(list);
+        
+        list.remove('a');
+
+        // boolean flag = list.remove(Character.valueOf('a')); // succesfully removed value
+        // System.out.println(flag);
+        System.out.println(list);
+
+        
+
+    }
+}
